9aa4623e5e5e3902bed04e92c7ab9fd3b96a07a1
Afsanab <abhuiyan8983@bths.edu>
2025-11-10 01:37:30 -0500

    todos workingggg

--- todo.js
+++ todo.js
@@ -1,5 +1,21 @@
 let input = prompt("what would you like to do?")
-while (input != "quit") {
+const todos = ["collect eggs", "clean litter box"]
+while (input != "quit" && input != "q") {
+    if (input === "list") {
+        console.log("**************")
+        for (let i = 0; i < todos.length; i++) {
+            console.log(`${i}: ${todos[i]}`);
+        }
+        console.log("**************")
+    } else if (input === 'new') {
+        const newTodo = prompt("Ok, what is the new todo?")
+        todos.push(newTodo);
+        console.log(`${newTodo} added to list`);
+    }
     input = prompt("what would you like to do?")
 }
 console.log("Ok, You quit the app!")
+// coding is fun you just hate thinking now
+// because your so addicted to your phone
+// the only way to fix this is to keep pushing your limit and endurance
+// rewire your brain to be able to study for longer and longer
