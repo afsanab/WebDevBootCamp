a3e03421627279c7f9ffce8ed30061c8420ab352
Afsanab <abhuiyan8983@bths.edu>
2025-11-11 23:22:59 -0500

    higher order functions

--- functions.js
+++ functions.js
@@ -1,3 +1,17 @@
-function greet(name) {
-    console.log(`Hi ${name}`);
+// function greet(name) {
+//     console.log(`Hi ${name}`);
+// }
+//  trying methods out - functions within objects
+const myMath = {
+    num: 67,
+    square(n) {
+        return n * n;
+    },
+    cube(n) {
+        return n ** 3;
+    }
 }
+
+// would be run like this
+myMath.square(7);
+
