927dd3ae55e465285d80adbefba64ae8866d7fb8
Afsanab <abhuiyan8983@bths.edu>
2026-01-12 12:08:54 -0500

    destructuring

---
+++ newer_js_features.js
@@ -0,0 +1,20 @@
+//note: this section didn't have any quizzes so im going back to practice some of the topics to make sure i understood them
+
+// default params
+
+//spread in function
+
+//spread in array literals
+
+//spread in objects
+
+//rest params
+
+// destructuring arrays
+
+const winners = ['Neymar', 'Messi', 'Ronaldo', 'Mbappe'];
+const [gold, bronze] = winners;
+
+// destructuring objects
+
+// destructuring params
