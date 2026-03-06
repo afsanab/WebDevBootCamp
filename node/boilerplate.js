0fc74db39b037b942585e47231b967d9fd6fe63f
Afsanab <abhuiyan8983@bths.edu>
2026-03-05 21:31:43 -0500

    messing with files with functions sync and async

--- node/boilerplate.js
+++ node/boilerplate.js
@@ -1,9 +1,14 @@
 const fs = require('fs');
-fs.mkdir('Dogs', { recursive: true }, (err) => {
-    console.log("IN THE CLLBACK")
-    if (err) {
-        console.log(err);
-    };
-});
+const foldername = process.argv[2] || 'Project';
+// fs.mkdir('Dogs', { recursive: true }, (err) => {
+//     console.log("IN THE CLLBACK")
+//     if (err) {
+//         console.log(err);
+//     };
+// });
 
-console.log("AFTER"); 
+fs.mkdirSync(foldername);
+fs.writeFileSync(`${foldername}/index.html`, '');
+fs.writeFileSync(`${foldername}/app.js`, '');
+fs.writeFileSync(`${foldername}/styles.css`, '');
+
