9d36a2f64e22058a0db7dd48258bf59a4c5c85c9
Afsanab <abhuiyan8983@bths.edu>
2026-03-06 01:06:16 -0500

    strat of making mods

---
+++ node/math.js
@@ -0,0 +1,24 @@
+const add = (x, y) => x + y;
+
+
+const PI = 3.14159;
+
+const square = x => x * x;
+
+// module.exports.add = add;
+// module.exports.PI = PI;
+// module.exports.square = square;
+
+// module.exports = {
+//     add,
+//     PI,
+//     square
+// }
+
+const math = {
+    add,
+    PI,
+    square
+}
+
+module.exports = math;
