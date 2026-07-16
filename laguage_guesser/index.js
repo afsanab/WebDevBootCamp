51a8733144555bd3360076b6e27ab3e0ee4ebd6e
Afsanab <abhuiyan8983@bths.edu>
2026-07-15 21:47:56 -0400

    add initial project files including .gitignore, package.json, package-lock.json, and index.js

---
+++ laguage_guesser/index.js
@@ -0,0 +1,9 @@
+// TODO:
+//  take in a command line arg node index.js "arg"
+//  then print the name of the language
+//  langs.where
+//  create a package
+// franc gives iso lang code, that will  read both documentation 
+
+const franc = require('franc');
+
