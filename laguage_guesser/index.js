0796433ae6b8b9eb645c69fc73664f52166bf9a1
Afsanab <abhuiyan8983@bths.edu>
2026-07-15 22:31:18 -0400

    update dependencies and implement language detection functionality

--- laguage_guesser/index.js
+++ laguage_guesser/index.js
@@ -6,4 +6,8 @@
 // franc gives iso lang code, that will  read both documentation 
 
 const franc = require('franc');
+const langs = require('langs');
+args = process.argv;
 
+console.log(args[2]);
+console.log(langs.where("3", franc(args[2])).name);
