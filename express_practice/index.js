98e611989c30b5754e7598cb1938a1924acccd89
Afsanab <abhuiyan8983@bths.edu>
2026-07-16 19:23:54 -0400

    Add request handling and response functionality to the Express server

--- express_practice/index.js
+++ express_practice/index.js
@@ -1,3 +1,13 @@
 const express = require('express');
 const app = express();
-console.log(express);
+// this is going to print in the command line 
+// everythime you get a new request
+// which is everytime you refresh the localhost:3000
+app.use((req, res) => {
+    console.log('Hello World!')
+    res.send('Hello we are live!');
+})
+
+// this prints once when the server starts
+app.listen(3000, () => console.log('Server is listening on port 3000'));
+
